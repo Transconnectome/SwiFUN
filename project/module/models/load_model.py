11a97f628ecf95c2de23cdf6c05ffedca74a4265
Junebeomstics <kjb961013@naver.com>
2024-10-07 18:58:00 -0700

    added brainvolcnn

--- project/module/models/load_model.py
+++ project/module/models/load_model.py
@@ -1,9 +1,6 @@
 from .swin4d_transformer_ver7 import SwinTransformer4D as SwinTransformer4D_ver7
-# from .mae_swin4d_transformer_ver7 import MaskedAutoencoderSwinTransformer4D as MaskedAutoencoderSwinTransformer4D_ver7
-# from .mae_swin4d_transformer_ver8 import MaskedAutoencoderSwinTransformer4D as MaskedAutoencoderSwinTransformer4D_ver8
-# from .mae_swin4d_transformer_ver9 import MaskedAutoencoderSwinTransformer4D as MaskedAutoencoderSwinTransformer4D_ver9
-#from monai.networks.nets import SwinUNETR
 from .swinunetr_og import SwinUNETR, SwinUNETR_encoder
+from .brain_surf_cnn import BrainSurfCNN3D
 
 def load_model(model_name, hparams=None):
     #number of transformer stages
@@ -41,10 +38,32 @@ def load_model(model_name, hparams=None):
             dropout_path_rate=hparams.attn_drop_rate,
             attn_drop_rate=hparams.attn_drop_rate,
             feature_size=hparams.embed_dim,
-            use_v2=hparams.use_v2) 
+            use_v2=hparams.use_v2)
+    elif model_name.lower() == "swin4d_ver7":
+        net = SwinTransformer4D_ver7(
+            img_size=hparams.img_size,
+            in_chans=hparams.in_chans,
+            embed_dim=hparams.embed_dim,
+            window_size=hparams.swift_window_size,
+            first_window_size=hparams.swift_window_size,
+            patch_size=hparams.swift_patch_size,
+            depths=hparams.depths,
+            num_heads=hparams.num_heads,
+            c_multiplier=hparams.c_multiplier,
+            last_layer_full_MSA=hparams.last_layer_full_MSA,
+            to_float = to_float,
+            drop_rate=hparams.attn_drop_rate,
+            drop_path_rate=hparams.attn_drop_rate,
+            attn_drop_rate=hparams.attn_drop_rate
+        )
+    elif model_name.lower() == "brainsurfcnn3d":
+        net = BrainSurfCNN3D(in_ch=hparams.sequence_length, out_ch=1)
     elif model_name == "emb_mlp":
         from .emb_mlp import mlp
         net = mlp(final_embedding_size=128, num_tokens = hparams.embed_dim * (hparams.c_multiplier ** (n_stages - 1)), use_normalization=True)
+    elif model_name == "emb_mlp_swin4d":
+        from .emb_mlp import mlp
+        net = mlp(final_embedding_size=132032, num_tokens = hparams.embed_dim * (hparams.c_multiplier ** (n_stages - 1)), use_normalization=True)
     elif model_name == "clf_mlp":
         if hparams.clf_head_version == 'v1':
             from .clf_mlp import mlp
